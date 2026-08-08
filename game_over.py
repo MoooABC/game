d3c505d078b094db42f7c6c1d73d403201446465
Alon <alonishi.harel@gmail.com>
2026-08-08 20:24:33 +0300

    created game over mechanic

---
+++ game_over.py
@@ -0,0 +1,47 @@
+import pygame
+import pygame_gui
+
+def game_over(time):
+    pygame.init()
+
+    window_surface = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Game Over")
+
+    background = pygame.Surface((800, 600))
+    background.fill(pygame.Color('#282c34'))
+
+    ui_manager = pygame_gui.UIManager(
+        (800, 600),
+        theme_path={"label": {"font": {"name": "arial", "size": "30"}}}
+    )
+
+    label_lose = pygame_gui.elements.UILabel(
+        relative_rect=pygame.Rect((300, 250), (200, 50)),
+        text= f"You lost!",
+        manager=ui_manager
+    )
+
+    label_feedback = pygame_gui.elements.UILabel(
+        relative_rect=pygame.Rect((300, 270), (200, 70)),
+        text= "you are a loser!" if time < 10 else "you can do better" if time < 100 else "geat! :)",
+        manager=ui_manager
+    )
+
+
+    clock = pygame.time.Clock()
+    is_running = True
+
+    while is_running:
+        time_delta = clock.tick(60) / 1000.0
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                is_running = False
+
+            ui_manager.process_events(event)
+        ui_manager.update(time_delta)
+
+        window_surface.blit(background, (0, 0))
+        ui_manager.draw_ui(window_surface)
+
+        pygame.display.update()
--- main.py
+++ main.py
@@ -1,19 +1,40 @@
-import pygame, sys
+import pygame, sys, time, pygame_gui
+
 from player import Player
 from very_stupid_enemy import very_stupid_enemy
+from game_over import game_over
 
+pygame.init()
 W, H = 800, 600
 screen = pygame.display.set_mode((W, H))
-
 pygame.display.set_caption("(-:")
+
+ui_manager = pygame_gui.UIManager(
+    (800, 600),
+    theme_path={"label": {"font": {"name": "arial", "size": "30"}}}
+)
+label_time = pygame_gui.elements.UILabel(
+    relative_rect=pygame.Rect((0, 0), (100, 100)),
+    text= f"000.0s",
+    manager=ui_manager
+)
+
 clock = pygame.time.Clock()
+player = None
+enemies = None
+start_time = None
 
+def reset():
+    global player, enemies, start_time
+    player = Player(W/2, H/2, 10, 5)
+    enemies = [very_stupid_enemy.generate_enemy(W, H) for _ in range(40)]
+    start_time = time.time()
 
-player = Player(W/2, H/2, 10, 10)
-enemies = [very_stupid_enemy.generate_enemy(W, H) for _ in range(10)]
-
+reset()
 while True:
+    time_delta = clock.tick(60) / 1000.0
     player.reset_for_next_frame()
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -23,8 +44,11 @@ while True:
             if event.key == pygame.K_SPACE:
                 pygame.quit()
                 sys.exit()
-
+        ui_manager.process_events(event)
     screen.fill((255, 255, 255))
+    label_time.set_text(str(round(time.time()-start_time, 1)))
+    ui_manager.update(time_delta)
+    ui_manager.draw_ui(screen)
 
     keys = pygame.key.get_pressed()
     player.handel_movement(keys)
@@ -33,9 +57,8 @@ while True:
     for e in enemies:
         e.handel_movement()
         if (e.check_collision_with_player(player)):
-            print("Game Over")
-            pygame.quit()
-            sys.exit()
+            game_over(time.time()-start_time)
+            reset()
 
         e.draw(screen)
         if e.update(screen) == -1:
@@ -45,5 +68,4 @@ while True:
 
 
     pygame.display.flip()
-
-    clock.tick(60)
+    pygame.display.update()
